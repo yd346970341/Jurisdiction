67fc8a772d46fd385e4af245ec9b398ad0f895be
yd346970341 <346970341@qq.com>
2018-03-09 22:03:20 +0800

    first commit

---
+++ src/com/gpedu/homework/single/SingleTest.java
@@ -0,0 +1,13 @@
+package com.gpedu.homework.single;
+
+/**
+ * Created by Administrator on 2018/3/9.
+ */
+public class SingleTest {
+
+    public static void main(String[] args) {
+        System.out.println("hellow world!");
+        System.out.println("hellow world!");
+        System.out.println("hellow world!");
+    }
+}
